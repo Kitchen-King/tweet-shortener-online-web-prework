adf6a452f7e4838d3001f6e0a6e734c9d9e8ba2f
Kyle Spencer <spencerk2@vcu.edu>
2019-02-09 04:17:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+def word_substituter(tweet)
+  
+  dictionary = {
+"hello" => "hi",
+"to" => "2",
+"two" => "2",
+"too" => "2",
+"for" => "4",
+"four" => "4",
+"be" => "b",
+"you" => "u",
+"at" => "@",
+"and" => "&"
+}
+
+  tweetarray = tweet.split(" ")
+    dictionary.each do |word, shortword|
+  a = word
+  b = shortword  
+  c = tweetarray.index(word)  
+  tweetarray.collect do |tword|
+    if tword == a then replacementword = b
+    tweetarray[c] = b
+    binding.pry
+        end
+      end
+    end
+  end
